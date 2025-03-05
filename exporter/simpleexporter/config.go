477d10bf2d7d9802269e863e0e70316a32971940
liuxiao <xxz2535737692@gmail.com>
2025-03-05 11:18:51 +0800

    simpleexporter init

--- exporter/simpleexporter/config.go
+++ exporter/simpleexporter/config.go
@@ -1,10 +1,42 @@
 package simpleexporter
 
-import "go.opentelemetry.io/collector/component"
+import (
+	"errors"
+	"go.opentelemetry.io/collector/config/confighttp"
+	"go.opentelemetry.io/collector/config/configretry"
+	"go.opentelemetry.io/collector/exporter/exporterhelper"
+)
 
+type EncodingType string
+
+const (
+	EncodingProto EncodingType = "proto"
+	EncodingJSON  EncodingType = "json"
+)
+
+// Config defines configuration for JVM/HTTP exporter.
 type Config struct {
+	confighttp.ClientConfig    `mapstructure:",squash"` // squash ensures fields are correctly decoded in embedded struct.
+	exporterhelper.QueueConfig `mapstructure:"sending_queue"`
+	RetryConfig                configretry.BackOffConfig `mapstructure:"retry_on_failure"`
+
+	// The URL to send traces to. If omitted the Endpoint + "/v1/traces" will be used.
+	TracesEndpoint string `mapstructure:"traces_endpoint"`
+
+	// The URL to send metrics to. If omitted the Endpoint + "/v1/metrics" will be used.
+	MetricsEndpoint string `mapstructure:"metrics_endpoint"`
+
+	// The URL to send logs to. If omitted the Endpoint + "/v1/logs" will be used.
+	LogsEndpoint string `mapstructure:"logs_endpoint"`
+
+	// The encoding to export telemetry (default: "proto")
+	Encoding EncodingType `mapstructure:"encoding"`
 }
 
-func createDefaultConfig() component.Config {
-	return &Config{}
+func (cfg *Config) Validate() error {
+	logger.Info("Validate config .......")
+	if cfg.Endpoint == "" && cfg.TracesEndpoint == "" && cfg.MetricsEndpoint == "" && cfg.LogsEndpoint == "" {
+		return errors.New("at least one endpoint must be specified")
+	}
+	return nil
 }
--- exporter/simpleexporter/factory.go
+++ exporter/simpleexporter/factory.go
@@ -2,12 +2,19 @@ package simpleexporter
 
 import (
 	"context"
+	"fmt"
 	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/config/configcompression"
+	"go.opentelemetry.io/collector/config/confighttp"
+	"go.opentelemetry.io/collector/config/configretry"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/exporter"
 	"go.opentelemetry.io/collector/exporter/exporterhelper"
 	"go.opentelemetry.io/collector/exporter/xexporter"
 	"go.uber.org/zap"
+	"net/url"
+	"strings"
+	"time"
 )
 
 var (
@@ -31,18 +38,61 @@ func NewFactory() exporter.Factory {
 	)
 }
 
+func createDefaultConfig() component.Config {
+	logger.Info("jvmhttp exporter createDefaultConfig ......")
+	clientConfig := confighttp.NewDefaultClientConfig()
+	clientConfig.Timeout = 30 * time.Second
+	// Default to gzip compression
+	clientConfig.Compression = configcompression.TypeGzip
+	// We almost read 0 bytes, so no need to tune ReadBufferSize.
+	clientConfig.WriteBufferSize = 512 * 1024
+
+	return &Config{
+		RetryConfig:  configretry.NewDefaultBackOffConfig(),
+		QueueConfig:  exporterhelper.NewDefaultQueueConfig(),
+		Encoding:     EncodingProto,
+		ClientConfig: clientConfig,
+	}
+}
+
 func createMetrics(ctx context.Context, set exporter.Settings, cfg component.Config) (exporter.Metrics, error) {
-	logger.Info("jvmhttp exporter started")
+	logger.Info("simple exporter started")
 	oce, err := newExporter(cfg, set)
 	if err != nil {
 		return nil, err
 	}
+	oCfg := cfg.(*Config)
+
+	oce.metricsURL, err = composeSignalURL(oCfg, oCfg.MetricsEndpoint, "metrics", "v1")
+	if err != nil {
+		return nil, err
+	}
+
 	return exporterhelper.NewMetrics(ctx, set, cfg,
 		oce.pushMetrics,
 		exporterhelper.WithStart(oce.start),
 		exporterhelper.WithCapabilities(consumer.Capabilities{MutatesData: false}),
 		// explicitly disable since we rely on http.Client timeout logic.
 		exporterhelper.WithTimeout(exporterhelper.TimeoutConfig{Timeout: 0}),
-	)
+		exporterhelper.WithRetry(oCfg.RetryConfig),
+		exporterhelper.WithQueue(oCfg.QueueConfig))
 
+}
+
+func composeSignalURL(oCfg *Config, signalOverrideURL string, signalName string, signalVersion string) (string, error) {
+	switch {
+	case signalOverrideURL != "":
+		_, err := url.Parse(signalOverrideURL)
+		if err != nil {
+			return "", fmt.Errorf("%s_endpoint must be a valid URL", signalName)
+		}
+		return signalOverrideURL, nil
+	case oCfg.Endpoint == "":
+		return "", fmt.Errorf("either endpoint or %s_endpoint must be specified", signalName)
+	default:
+		if strings.HasSuffix(oCfg.Endpoint, "/") {
+			return oCfg.Endpoint + signalVersion + "/" + signalName, nil
+		}
+		return oCfg.Endpoint + "/" + signalVersion + "/" + signalName, nil
+	}
 }
