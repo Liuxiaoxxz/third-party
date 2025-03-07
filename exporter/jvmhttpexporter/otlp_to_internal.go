f241ca0cebf218f8f529f3db64717e6785873d28
liuxiao <xxz2535737692@gmail.com>
2025-03-07 12:37:21 +0800

    jvmexporter otlp transform

--- exporter/jvmhttpexporter/otlp_to_internal.go
+++ exporter/jvmhttpexporter/otlp_to_internal.go
@@ -133,7 +133,7 @@ func metricTransform(ctx context.Context, md pmetric.Metrics) ([]byte, error) {
 				msLen := metrics.Len()
 				for i := 0; i < msLen; i++ {
 					metric := metrics.At(i)
-					copeMetricv1(data, metric)
+					copeMetricV2(data, metric)
 				}
 			}
 		}
@@ -229,7 +229,7 @@ func copeMetric(data *InternalData, metric pmetric.Metric) {
 
 }
 
-func copeMetricv1(data *InternalData, metric pmetric.Metric) {
+func copeMetricV1(data *InternalData, metric pmetric.Metric) {
 	switch metric.Name() {
 	case JVM_MEMORY_USED, JVM_MEMORY_COMMITTED, JVM_MEMORY_LIMITI:
 		dataPoints := metric.Sum().DataPoints()
@@ -267,6 +267,80 @@ func copeMetricv1(data *InternalData, metric pmetric.Metric) {
 				garbageCollectorName := dict[name.AsString()]
 				if _, exists := data.GarbageCollector.GarbageCollectors[garbageCollectorName]; !exists {
 					// 初始化 GarbageCollectorInfo
+					data.GarbageCollector.GarbageCollectors[garbageCollectorName] = GarbageCollectorInfo{}
+				}
+				garbageCollectorInfo := data.GarbageCollector.GarbageCollectors[garbageCollectorName]
+				garbageCollectorInfo.Name = garbageCollectorName
+				garbageCollectorInfo.CollectionCount = dataPoint.Count()
+				garbageCollectorInfo.CollectionTime = int(dataPoint.Sum() * 1000)
+			}
+		}
+	case JVM_THREAD_COUNT:
+		sum := metric.Sum()
+		dataPoints := sum.DataPoints()
+		var threadCount int64
+		var daemonThreadCount int64
+		for i := 0; i < dataPoints.Len(); i++ {
+			dataPoint := dataPoints.At(i)
+			currentThreadCount := dataPoint.IntValue()
+			threadCount += currentThreadCount
+			isDaemon, b := dataPoint.Attributes().Get(JVM_THREAD_DAEMON)
+			if b && isDaemon.Bool() {
+				daemonThreadCount += currentThreadCount
+			}
+		}
+		data.Thread.ThreadCount = threadCount
+		data.Thread.PeakThreadCount = threadCount
+		data.Thread.DeamonThreadCount = daemonThreadCount
+	}
+}
+
+func copeMetricV2(data *InternalData, metric pmetric.Metric) {
+	// 确保 MemoryPool 和 GarbageCollector 的 maps 被初始化
+	if data.MemoryPool.MemoryUsages == nil {
+		data.MemoryPool.MemoryUsages = make(map[string]MemoryUsage)
+	}
+	if data.GarbageCollector.GarbageCollectors == nil {
+		data.GarbageCollector.GarbageCollectors = make(map[string]GarbageCollectorInfo)
+	}
+
+	switch metric.Name() {
+	case JVM_MEMORY_USED, JVM_MEMORY_COMMITTED, JVM_MEMORY_LIMITI:
+		dataPoints := metric.Sum().DataPoints()
+		for i := 0; i < dataPoints.Len(); i++ {
+			dataPoint := dataPoints.At(i)
+			dataPointAttributes := dataPoint.Attributes()
+			v, b := dataPointAttributes.Get(JVM_MEMORY_POOL_NAME)
+			if b {
+				poolName := v.AsString()
+				// 获取池名映射
+				mappedName := dict[poolName]
+				// 确保内存池数据被初始化
+				if _, exists := data.MemoryPool.MemoryUsages[mappedName]; !exists {
+					data.MemoryPool.MemoryUsages[mappedName] = MemoryUsage{}
+				}
+				// 根据字段填充数据
+				memoryUsage := data.MemoryPool.MemoryUsages[mappedName]
+				if metric.Name() == JVM_MEMORY_USED {
+					memoryUsage.Used = dataPoint.IntValue()
+				} else if metric.Name() == JVM_MEMORY_COMMITTED {
+					memoryUsage.Committed = dataPoint.IntValue()
+				} else if metric.Name() == JVM_MEMORY_LIMITI {
+					memoryUsage.Max = dataPoint.IntValue()
+				}
+			}
+		}
+	case JVM_GC_DURATION:
+		histogram := metric.Histogram()
+		dataPoints := histogram.DataPoints()
+		for i := 0; i < dataPoints.Len(); i++ {
+			dataPoint := dataPoints.At(i)
+			dataPointAttributes := dataPoint.Attributes()
+			name, b := dataPointAttributes.Get(JVM_GC_NAME)
+			if b {
+				garbageCollectorName := dict[name.AsString()]
+				// 确保垃圾收集器数据被初始化
+				if _, exists := data.GarbageCollector.GarbageCollectors[garbageCollectorName]; !exists {
 					data.GarbageCollector.GarbageCollectors[garbageCollectorName] = GarbageCollectorInfo{}
 				}
 				garbageCollectorInfo := data.GarbageCollector.GarbageCollectors[garbageCollectorName]
