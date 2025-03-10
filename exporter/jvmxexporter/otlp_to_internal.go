75675878dbcf731408392371ceb11c71c53b16a1
liuxiao <xxz2535737692@gmail.com>
2025-03-10 16:49:03 +0800

    metrics grpc-client

--- exporter/jvmxexporter/otlp_to_internal.go
+++ exporter/jvmxexporter/otlp_to_internal.go
@@ -71,6 +71,12 @@ func metricTransform(ctx context.Context, md pmetric.Metrics) (*metrics.ExportMe
 
 func copeMetric(data *metrics.ExportMetricsServiceRequest, metric pmetric.Metric) {
 	// 确保 MemoryPool 和 GarbageCollector 的 maps 被初始化
+	if data.MemoryPool == nil {
+		data.MemoryPool = &metrics.MemoryPool{}
+	}
+	if data.GarbageCollector == nil {
+		data.GarbageCollector = &metrics.GarbageCollector{}
+	}
 	if data.MemoryPool.MemoryUsages == nil {
 		data.MemoryPool.MemoryUsages = make(map[string]*metrics.MemoryUsage)
 	}
@@ -152,3 +158,102 @@ func copeMetric(data *metrics.ExportMetricsServiceRequest, metric pmetric.Metric
 		data.Thread.DeamonThreadCount = daemonThreadCount
 	}
 }
+
+//func copeMetric(data *metrics.ExportMetricsServiceRequest, metric pmetric.Metric) {
+//	// 确保 MemoryPool 和 GarbageCollector 的 maps 被初始化
+//	if data.MemoryPool == nil {
+//		data.MemoryPool = &metrics.MemoryPool{}
+//	}
+//	if data.GarbageCollector == nil {
+//		data.GarbageCollector = &metrics.GarbageCollector{}
+//	}
+//	if data.MemoryPool.MemoryUsages == nil {
+//		data.MemoryPool.MemoryUsages = make(map[string]*metrics.MemoryUsage)
+//	}
+//	if data.GarbageCollector.GarbageCollectors == nil {
+//		data.GarbageCollector.GarbageCollectors = make(map[string]*metrics.GarbageCollectorInfo)
+//	}
+//
+//	switch metric.Name() {
+//	case JVM_MEMORY_USED, JVM_MEMORY_COMMITTED, JVM_MEMORY_LIMITI:
+//		dataPoints := metric.Sum().DataPoints()
+//		for i := 0; i < dataPoints.Len(); i++ {
+//			dataPoint := dataPoints.At(i)
+//			dataPointAttributes := dataPoint.Attributes()
+//
+//			// 在获取属性之前，确保 dataPointAttributes 不为 nil
+//			if dataPointAttributes != nil {
+//				v, b := dataPointAttributes.Get(JVM_MEMORY_POOL_NAME)
+//				if b {
+//					poolName := v.AsString()
+//					// 获取池名映射
+//					mappedName := dict[poolName]
+//					if mappedName == "" {
+//						// 如果映射不到，使用 poolName 本身作为默认值
+//						mappedName = poolName
+//					}
+//					// 确保内存池数据被初始化
+//					if _, exists := data.MemoryPool.MemoryUsages[mappedName]; !exists {
+//						data.MemoryPool.MemoryUsages[mappedName] = &metrics.MemoryUsage{
+//							Max: -1,
+//						}
+//					}
+//					// 根据字段填充数据
+//					memoryUsage := data.MemoryPool.MemoryUsages[mappedName]
+//					switch metric.Name() {
+//					case JVM_MEMORY_USED:
+//						memoryUsage.Used = dataPoint.IntValue()
+//					case JVM_MEMORY_COMMITTED:
+//						memoryUsage.Committed = dataPoint.IntValue()
+//					case JVM_MEMORY_LIMITI:
+//						memoryUsage.Max = dataPoint.IntValue()
+//					}
+//				}
+//			}
+//		}
+//	case JVM_GC_DURATION:
+//		histogram := metric.Histogram()
+//		dataPoints := histogram.DataPoints()
+//		for i := 0; i < dataPoints.Len(); i++ {
+//			dataPoint := dataPoints.At(i)
+//			dataPointAttributes := dataPoint.Attributes()
+//
+//			// 在获取属性之前，确保 dataPointAttributes 不为 nil
+//			if dataPointAttributes != nil {
+//				name, b := dataPointAttributes.Get(JVM_GC_NAME)
+//				if b {
+//					garbageCollectorName := dict[name.AsString()]
+//					if garbageCollectorName == "" {
+//						// 如果映射不到，使用 poolName 本身作为默认值
+//						garbageCollectorName = name.AsString()
+//					}
+//					// 确保垃圾收集器数据被初始化
+//					if _, exists := data.GarbageCollector.GarbageCollectors[garbageCollectorName]; !exists {
+//						data.GarbageCollector.GarbageCollectors[garbageCollectorName] = &metrics.GarbageCollectorInfo{}
+//					}
+//					garbageCollectorInfo := data.GarbageCollector.GarbageCollectors[garbageCollectorName]
+//					garbageCollectorInfo.Name = garbageCollectorName
+//					garbageCollectorInfo.CollectionCount = dataPoint.Count()
+//					garbageCollectorInfo.CollectionTime = int32(dataPoint.Sum() * 1000)
+//				}
+//			}
+//		}
+//	case JVM_THREAD_COUNT:
+//		sum := metric.Sum()
+//		dataPoints := sum.DataPoints()
+//		var threadCount int64
+//		var daemonThreadCount int64
+//		for i := 0; i < dataPoints.Len(); i++ {
+//			dataPoint := dataPoints.At(i)
+//			currentThreadCount := dataPoint.IntValue()
+//			threadCount += currentThreadCount
+//			isDaemon, b := dataPoint.Attributes().Get(JVM_THREAD_DAEMON)
+//			if b && isDaemon.Bool() {
+//				daemonThreadCount += currentThreadCount
+//			}
+//		}
+//		data.Thread.ThreadCount = threadCount
+//		data.Thread.PeakThreadCount = threadCount
+//		data.Thread.DeamonThreadCount = daemonThreadCount
+//	}
+//}
